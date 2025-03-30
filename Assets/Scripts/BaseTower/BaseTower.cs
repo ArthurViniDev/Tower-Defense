3d7551c8cf2bdfc1352a6dffb4a4b36d5ae302a6
ArthurViniDev <kenzygamedev@gmail.com>
2025-03-30 13:07:33 -0300

    level modifies

---
+++ Assets/Scripts/BaseTower/BaseTower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public abstract class BaseTower : MonoBehaviour
+{
+    public float damage;
+    public float attackSpeed;
+    public float range;
+    public int price;
+
+    protected GameObject Target;
+
+    public void SetTarget(GameObject newTarget)
+    {
+        Target = newTarget;
+    }
+
+    protected abstract void Attack();
+}

464836bce2a2f4f2b2f4038a5dd712cd3082ed3b
ArthurViniDev <kenzygamedev@gmail.com>
2025-04-16 16:34:22 -0300

    some fixes

--- Assets/Scripts/UI/Weapons/WeaponWindow.cs
+++ Assets/Scripts/UI/Weapons/WeaponWindow.cs
@@ -1,16 +1,21 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponWindow : MonoBehaviour
 {
-    private BaseWeapon _baseWeapon;
+    [Header("Base Weapon Screen")]
     private bool isWeaponScreenOpen;
-    [SerializeField] private GameObject weaponScreen;
+    private GameObject weaponScreen;
+    private BaseWeapon _baseWeapon;
+    [SerializeField] private Button refundButton;
 
     private void Awake()
     {
         _baseWeapon = GetComponent<BaseWeapon>();
         weaponScreen = transform.GetChild(1).gameObject;
         isWeaponScreenOpen = false;
+        refundButton.GetComponentInChildren<TextMeshProUGUI>().text = $"Refund (${_baseWeapon.refundValue})";
     }
 
     private void OnMouseOver()
